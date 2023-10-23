59f7cdde5fa6af96a22bb8c47c416b702f679f00
elisiomualumene <elisiomualumene@gmail.com>
2023-10-23 01:07:56 +0100

    :fire: validating entire entity

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- src/domain/video_test.go
+++ src/domain/video_test.go
@@ -2,8 +2,11 @@ package domain_test
 
 import (
 	"testing"
+	"time"
+
 	"github.com/elisiomualumene/video_encoder/src/domain"
 	"github.com/stretchr/testify/require"
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestValidateIfVideoIsEmpty(t *testing.T){
@@ -12,4 +15,30 @@ func TestValidateIfVideoIsEmpty(t *testing.T){
 	err := video.Validate()
 
 	require.Error(t, err)
+}
+
+func TestVideoIdIsNotUuid(t *testing.T){
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceId = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIsValid(t *testing.T){
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.ResourceId = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Nil(t, err)
 }
